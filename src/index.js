e7c36a21a263d7e636a8af893e14d32c696aff1d
Shirsho Dipto <shirshodipto@gmail.com>
2022-12-04 19:01:10 +0600

    Add todo modal add and cancel functionality

--- src/index.js
+++ src/index.js
@@ -1,34 +1,70 @@
 import List from './list'
+import Todo from './todo'
 import {addNewListInterface} from './manageListInterface'
 
 
 
 function addList(e) {
-    if (newListinput.value === '') return
     const newList = new List(newListinput.value, []);
     addNewListInterface(newList, allList, newListModal);
     console.log(newListModal);
     console.log(newList);
-    const listForm = document.querySelector('.new-list-modal form');
-    listForm.reset();
+    e.target.reset();
     newListModal.setAttribute('id', 'hide');
+    e.preventDefault()
+}
+
+
+function add_a_Todo(e) {
+    const newTodo = new Todo(todoName.value, description.value, dueDate.value, priority.value);
+    console.log(newTodo);
+    newTodoModal.setAttribute('id', 'hide');
+    e.target.reset();
     e.preventDefault()
 }
 
 
 const allList = document.querySelector('.all-list');
 const newListinput = document.querySelector('#list-name');
-const addListButton = document.querySelector('.add-list');
-const cancelListButton = document.querySelector('.cancel-list');
+const listForm = document.querySelector('.list-form');
 const addNewList = document.querySelector('.add-new-list');
 const newListModal = document.querySelector('.new-list-modal');
+const addTodo = document.querySelector('.add-todo');
+const newTodoModal = document.querySelector('.new-todo-modal');
+const todoForm = document.querySelector('.todo-form');
+const cancelTodo = document.querySelector('.cancel-todo');
+const todoName = document.querySelector('#todo-name');
+const dueDate = document.querySelector('#due-date');
+const priority = document.querySelector('#priority');
+const description = document.querySelector('#description');
+
+console.log(todoForm);
+console.log(cancelTodo);
+console.log(todoName);
+console.log(dueDate);
+console.log(priority);
+console.log(description);
+
+addTodo.onclick = () => {
+    newTodoModal.removeAttribute('id');
+}
 
 addNewList.addEventListener('click', () => {
     newListModal.removeAttribute('id');
 })
 
 
-addListButton.addEventListener('click', addList);
-cancelListButton.addEventListener('click', () => {
+listForm.onsubmit = addList;
+listForm.onreset = () => {
     newListModal.setAttribute('id', 'hide');
-});
+}
+
+
+// cancelListButton.addEventListener('click', () => {
+// });
+
+
+todoForm.onsubmit = add_a_Todo;
+todoForm.onreset = () => {
+    newTodoModal.setAttribute('id', 'hide');
+}
